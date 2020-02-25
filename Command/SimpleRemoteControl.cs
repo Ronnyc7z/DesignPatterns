ebcbad6cb48bdbc6495c4da0a1f7a8aada3404d3
ronny <ronny_c7z@yahoo.es>
2020-02-24 21:29:08 -0500

    feat(command): controller

---
+++ Command/SimpleRemoteControl.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Command
+{
+    public class SimpleRemoteControl
+    {
+        Command slot;
+
+        public void SetCommand(Command command)
+        {
+            slot = command;
+        }
+
+        public void PressButton()
+        {
+            slot.Execute();
+        }
+    }
+}
